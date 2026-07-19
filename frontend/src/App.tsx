cb56e87f0ecbffa0a62a4e553d0b7bdda1080004
lisy1202 <lidysjaraba9@gmail.com>
2026-07-19 17:57:19 -0500

    feat: conecta todas las rutas del modulo de autenticacion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,33 @@
-import { AuthProvider } from './features/auth/presentation/AuthProvider'
-import { AuthExperience } from './features/auth/presentation/AuthExperience'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { AuthProvider } from "./modules/auth/context/AuthProvider"
+import { AuthPage } from "./modules/auth/components/AuthPage"
+import { ForgotPasswordPage } from "./modules/auth/components/ForgotPasswordPage"
+import { ResetPasswordPage } from "./modules/auth/components/ResetPasswordPage"
+import { VerifyEmailPage } from "./modules/auth/components/VerifyEmail/VerifyEmailPage"
+import { AuthSuccessPage } from "./modules/auth/components/AuthSuccess/AuthSuccessPage"
+import { ActivateAccountPage } from "./modules/auth/components/ActivateAccount/ActivateAccountPage"
 
 
 function App() {
   return (
-    <AuthProvider>
-      <AuthExperience />
-    </AuthProvider>
+    
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+
+          <Route path="/login" element={<AuthPage/>} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage/>} />
+          <Route path="/reset-password" element={<ResetPasswordPage/>} />
+          <Route path="/verify-email" element={<VerifyEmailPage />} />
+          <Route path="/auth/success" element={<AuthSuccessPage />} />
+          <Route path="/activate-account" element={<ActivateAccountPage />} />
+
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          <Route path="*" element={<Navigate to="/login" replace/>} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
 
